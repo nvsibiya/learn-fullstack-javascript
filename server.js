14f8340c8c89dfcb4f7a96fbb87a1674662fbcc8
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-07-08 16:40:39 +0200

    https module

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-import config, { nodeEnv } from './config';
 
+import https from 'https';
+
+https.get('https://www.lynda.com', res => {
+    console.log('Response status code: ', res.statusCode);
+
+    res.on('data', chunk => {
+        console.log(chunk.toString());
+    });
+});
